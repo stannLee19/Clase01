6ab20128692d66c557b368c9e36dc8939aaaf540
stannLee19 <63014088+stannLee19@users.noreply.github.com>
2020-08-15 13:31:38 -0600

    Creacion de una clase dentro del paquete conexionmysql
    clase: ConexionBd

--- clase01130820/src/main/java/com/unab/edu/conexionmysql/ConexionBd.java
+++ clase01130820/src/main/java/com/unab/edu/conexionmysql/ConexionBd.java
@@ -3,21 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.unab.edu.Entidades;
-
-import lombok.Data;
+package com.unab.edu.conexionmysql;
 
 /**
  *
  * @author Stanly
  */
-@Data
-
-public class Persona {
-
-    private int idPersona;
-    private String Nombre;
-    private String Apellido;
-    private int Edad;
-    private String sexo;
+public class ConexionBd {
+    
 }
